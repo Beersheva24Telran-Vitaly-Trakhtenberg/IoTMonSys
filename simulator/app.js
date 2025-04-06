8ab0d9667a4d857fb9fd4e79ae0102f6d2f64053
Vitaly Trakhtenberg <webdev@tca.by>
2025-04-06 17:57:29 +0300

    Changed parameters of app.js: Short command in UPPERCASE Using default values instead direct access

--- simulator/app.js
+++ simulator/app.js
@@ -1,4 +1,4 @@
-const { program } = require('commander');
+const { program } = require('commander'); // CLI library
 const dotenv = require('dotenv');
 const path = require('path');
 const { createLogger } = require('./logger');
@@ -10,28 +10,64 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const logger = createLogger('simulator');
 
+const parseIntValue = (value) => {
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new Error('Value must be a number');
+  }
+  return parsedValue;
+};
+
+const parseFloatValue = (value) => {
+  const parsedValue = parseFloat(value);
+  if (isNaN(parsedValue)) {
+    throw new Error('Value must be a number(float)');
+  }
+  return parsedValue;
+};
+
+const DEFAULT_DEVICES = process.env.DEVICE_COUNT || 5;
+const DEFAULT_INTERVAL = process.env.SEND_INTERVAL || 5000;
+const DEFAULT_UDP_PORT = process.env.UDP_PORT || 41234;
+const DEFAULT_UDP_HOST = process.env.UDP_HOST || 'localhost';
+const DEFAULT_COMMAND_PORT = process.env.COMMAND_PORT || 41235;
+const DEFAULT_ANOMALY_RATE = process.env.ANOMALY_RATE || 5;
+
 program
   .version('0.0.1')
-  .option('-d, --devices <number>', 'Number of devices to generate', parseInt, 5)
-  .option('-i, --interval <number>', 'Interval for sending data (ms)', parseInt, 5000)
-  .option('-p, --udp-port <number>', 'UDP-port for sending data', parseInt, 41234)
-  .option('-h, --udp-host <string>', 'Host UDP-listener', 'localhost')
-  .option('-c, --command-port <number>', 'Port for receiving commands', parseInt, 41235)
-  .option('-a, --anomaly-rate <number>', 'Frequency of anomalies (0-100%)', parseFloat, 5)
-  .parse(process.argv);
+  .description('IoT Device UDP Simulator for testing IoT monitoring systems')
+  .option('-D, --devices <number>', 'Number of devices to generate', parseIntValue, DEFAULT_DEVICES)
+  .option('-I, --interval <number>', 'Interval for sending data (ms)', parseIntValue, DEFAULT_INTERVAL)
+  .option('-P, --udp-port <number>', 'UDP-port for sending data', parseIntValue, DEFAULT_UDP_PORT)
+  .option('-H, --udp-host <string>', 'Host UDP-listener', DEFAULT_UDP_HOST)
+  .option('-C, --command-port <number>', 'Port for receiving commands', parseIntValue, DEFAULT_COMMAND_PORT)
+  .option('-A, --anomaly-rate <number>', 'Frequency of anomalies (0-100%)', parseFloatValue, DEFAULT_ANOMALY_RATE);
+
+program.parse(process.argv);
 
 const options = program.opts();
 
+logger.debug('Command line arguments received:');
+logger.debug(JSON.stringify(options, null, 2));
+logger.debug('Raw process.argv:', process.argv);
+
+const deviceCount = options.devices || DEFAULT_DEVICES;
+const sendInterval = options.interval || DEFAULT_INTERVAL;
+const udpPort = options.udpPort || DEFAULT_UDP_PORT;
+const udpHost = options.udpHost || DEFAULT_UDP_HOST;
+const commandPort = options.commandPort || DEFAULT_COMMAND_PORT;
+const anomalyRate = options.anomalyRate || DEFAULT_ANOMALY_RATE;
+
 logger.info('Start of UDP-simulator of IoT devices');
 logger.info(`Number of devices: ${options.devices}`);
-logger.info(`Sending interval: ${options.interval} мс`);
+logger.info(`Sending interval: ${options.interval} ms`);
 logger.info(`UDP host: ${options.udpHost}:${options.udpPort}`);
 logger.info(`Port for command: ${options.commandPort}`);
 logger.info(`Frequency of anomalies: ${options.anomalyRate}%`);
 
-const deviceGenerator = new DeviceGenerator(options.devices, options.anomalyRate);
-const udpSender = new UdpSender(options.udpHost, options.udpPort);
-const commandReceiver = new CommandReceiver(options.commandPort);
+const deviceGenerator = new DeviceGenerator(deviceCount, anomalyRate);
+const udpSender = new UdpSender(udpHost, udpPort);
+const commandReceiver = new CommandReceiver(commandPort);
 
 commandReceiver.on('command', (command) => {
   logger.info(`Received command: ${JSON.stringify(command)}`);
@@ -64,7 +100,7 @@ function sendDataPeriodically() {
   });
 }
 
-const interval = setInterval(sendDataPeriodically, options.interval);
+const interval = setInterval(sendDataPeriodically, sendInterval);
 
 process.on('SIGINT', () => {
   logger.info('Simulator is stopping...');
