494f9bc240354866ded91980c41773052dea9d44
Vitaly Trakhtenberg <webdev@tca.by>
2025-04-25 12:08:07 +0300

    Fixing of the bug 'no deviceId found'.

--- lambdas/functions/src/main/java/optdev/iotmonsys/lambdas/DeviceAddedEventHandler.java
+++ lambdas/functions/src/main/java/optdev/iotmonsys/lambdas/DeviceAddedEventHandler.java
@@ -22,34 +22,31 @@ public class DeviceAddedEventHandler implements RequestStreamHandler {
         String eventJson = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))
                 .lines()
                 .reduce("", (acc, line) -> acc + line);
-        //logger.log("Event DeviceAdded: " + eventJson);
-        System.out.println("[EVENT] DeviceAdded: " + eventJson);
+        logger.log("[EVENT] DeviceAdded: " + eventJson);
 
         ObjectMapper mapper = new ObjectMapper();
         JsonNode rootNode = mapper.readTree(eventJson);
         JsonNode bodyJson = rootNode.get("body");
         if (bodyJson == null || bodyJson.isNull()) {
-            //logger.log("No body in given event");
-            System.err.println("[WARN] No body in given event");
+            logger.log("[WARN] No body in given event");
             return;
         }
 
-        String bodyJsonString = bodyJson.asText();
-        JsonNode bodyNode = mapper.readTree(bodyJsonString);
+        JsonNode bodyNode = mapper.readTree(bodyJson.asText());
+
         ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
         String prettyBody = writer.writeValueAsString(bodyNode);
-
         String emailText = "New event from IoTMonSys:\n" + prettyBody + "\n\n";
-        JsonNode deviceId = bodyJson.get("deviceId");
+
+        JsonNode deviceId = bodyNode.get("deviceId");
         if (deviceId == null || deviceId.isNull()) {
-            //logger.log("No deviceId in given event");
-            System.err.println("[ERROR] No deviceId in given event");
+            logger.log("[ERROR] No deviceId in given event");
         } else {
             String deviceIdString = deviceId.asText();
 
             String deviceManagementInitialString = "You can approve this device or remove this one. Or do nothing, device will wait in 'pending' status.\n";
-            String addDeviceString = "<a href=\"https://adding-device.com?deviceId=" + deviceId + "\">Click here to add</a> this device.\n";
-            String removeDeviceString = "<a href=\"https://remove-device.com?deviceId=" + deviceId + "\">Click here to remove</a> this device.\n";
+            String addDeviceString = "<a href=\"https://adding-device.com?deviceId=" + deviceIdString + "\">Click here to add</a> this device.\n";
+            String removeDeviceString = "<a href=\"https://remove-device.com?deviceId=" + deviceIdString + "\">Click here to remove</a> this device.\n";
             String deviceManagementString = deviceManagementInitialString +
                     addDeviceString + "\n" +
                     removeDeviceString;
@@ -61,8 +58,9 @@ public class DeviceAddedEventHandler implements RequestStreamHandler {
         if (topicArn != null && !topicArn.isEmpty()) {
             AmazonSNS snsClient = AmazonSNSClientBuilder.defaultClient();
             snsClient.publish(topicArn, emailText, "IoTMonSys Alert");
-            //logger.log("SNS notification sent.");
-            System.out.println("[DEBUG] SNS notification sent.");
+            logger.log("[DEBUG] SNS notification sent.");
+        } else {
+            logger.log("[ERROR] No SNS_TOPIC_ARN in environment");
         }
 
         String response = "{\"statusCode\":200,\"body\":\"OK\"}";
